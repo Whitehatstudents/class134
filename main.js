b8a7e0db883aa991dd67a5fbe7aff0fea8b2d3f3
Whitehatstudents <96421611+Whitehatstudents@users.noreply.github.com>
2022-02-02 13:51:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+song="";
+status1="";
+objects=[];
+function preload()
+{
+    song=loadSound("baby_crying.mp3");
+}
+function setup()
+{
+    canvas=createCanvas(380,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="status:detecting objects";
+}
+function modelLoaded()
+{
+    console.log("model loaded");
+    status1=true;
+}
+function draw()
+{
+    image(video,0,0,380,380);
+    if(status1!=""){
+        objectDetector.detect(video,gotResult);
+        for(i=0;i<objects.length;i++){
+            document.getElementById("status").innerHTML="status:objects detected"; 
+            fill("black");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x,objects[i].y);
+            noFill();
+            stroke("tan");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+            if(objects[i].label=="person"){
+                document.getElementById("oi2").innerHTML="baby found";
+                song.stop();
+            }
+            else{
+                document.getElementById("oi2").innerHTML="baby not found";
+                song.play();
+            }
+        }
+    }
+}
+function gotResult(error,results)
+{
+    if(error)  
+    {
+        console.error(error);
+    } 
+    else{
+        console.log(results);
+        objects=results;
+    }
+}
+
